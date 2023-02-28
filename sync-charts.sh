8229653cade91407be23a43530405aca40bb1a2c
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2023-02-28 19:21:04 +0100

    update version

--- sync-charts.sh
+++ sync-charts.sh
@@ -8,4 +8,5 @@ curl -J -L  https://github.com/highcanfly-club/crontab-ui/archive/HEAD.tar.gz | 
 helm package charts/crontab-ui
 curl -J -L  https://github.com/highcanfly-club/hcfmailer/archive/HEAD.tar.gz | tar xvz -C charts --strip 3 $TAR_WILDCARDS "*helm/hcfmailer*"
 helm package charts/hcfmailer
-mv crontab-ui-0.3.16-rc.2.tgz hcfmailer-2.3.9-rc.9.tgz repo/
+mv crontab-ui-*.tgz hcfmailer-*.tgz repo/
+helm repo index repo/ --url=https://helm-repo.highcanfly.club
