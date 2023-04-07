b7b6930665de63a14aff41cdbee75311d8407a4c
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2023-04-07 15:36:01 +0200

    dolidock

--- sync-charts.sh
+++ sync-charts.sh
@@ -8,5 +8,7 @@ curl -J -L  https://github.com/highcanfly-club/crontab-ui/archive/HEAD.tar.gz | 
 helm package charts/crontab-ui
 curl -J -L  https://github.com/highcanfly-club/hcfmailer/archive/HEAD.tar.gz | tar xvz -C charts --strip 3 $TAR_WILDCARDS "*helm/hcfmailer*"
 helm package charts/hcfmailer
-mv crontab-ui-*.tgz hcfmailer-*.tgz repo/
+curl -J -L https://github.com/ismogroup/dolidock/archive/HEAD.tar.gz | tar xvz -C charts --strip 2 $TAR_WILDCARDS "*helm/dolidock*"
+helm package charts/dolidock
+mv crontab-ui-*.tgz hcfmailer-*.tgz dolidock-*.tgz repo/
 helm repo index repo/ --url=https://helm-repo.highcanfly.club
