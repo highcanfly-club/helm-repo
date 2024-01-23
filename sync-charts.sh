2f94338d4c5a2937f530676f99ccb08b23089138
hcf coder <hcf@coder>
2024-01-23 14:01:53 +0000

    sort lines

--- sync-charts.sh
+++ sync-charts.sh
@@ -19,23 +19,23 @@ get_repo() {
 if [[ -n $1 && -n $2 && -n $3 ]]; then
         get_repo $1 $2 $3
 else
-        get_repo "highcanfly-club/crontab-ui" "helm/crontab-ui" 3
-        get_repo "ismogroup/dolidock" "helm/dolidock" 2
-        get_repo "highcanfly-club/cert-manager-webhook-oci" "cert-manager-webhook-oci" 2
-        get_repo "highcanfly-club/ubuntu-w64build" "helm/ubuntu-w64build" 2
-        get_repo "highcanfly-club/helm-dashboard" "helm/helm-dashboard" 2
-        get_repo "highcanfly-club/hcfmailer" "helm/hcfmailer" 3
-        get_repo "highcanfly-club/hcfmailer-plus" "helm/hcfmailerplus" 3
-        get_repo "highcanfly-club/easyappointments-k8s" "helm/easyappointments" 2
-        get_repo "highcanfly-club/hcfschedule" "helm/hcfschedule" 2
-        get_repo "highcanfly-club/pretix" "helm/pretix" 2
-        get_repo "highcanfly-club/helm-roundecubemail" "roundcube" 1
-        get_repo "highcanfly-club/hcf-coder" "helm/hcf-coder" 2
-        get_repo "highcanfly-club/odoo-bitnami-custom" "helm/odoo" 2
-        get_repo "highcanfly-club/gitea-bitnami-custom" "helm/gitea" 2
         get_repo "eltorio/minio-prometheus-chart" "helm/minio" 2
         get_repo "eltorio/whois-rest" "helm/whois-rest" 2
+        get_repo "highcanfly-club/cert-manager-webhook-oci" "cert-manager-webhook-oci" 2
+        get_repo "highcanfly-club/crontab-ui" "helm/crontab-ui" 3
         get_repo "highcanfly-club/docker-smtp-relay" "helm/flex-smtpd" 2
+        get_repo "highcanfly-club/easyappointments-k8s" "helm/easyappointments" 2
+        get_repo "highcanfly-club/gitea-bitnami-custom" "helm/gitea" 2
+        get_repo "highcanfly-club/hcf-coder" "helm/hcf-coder" 2
+        get_repo "highcanfly-club/hcfmailer-plus" "helm/hcfmailerplus" 3
+        get_repo "highcanfly-club/hcfmailer" "helm/hcfmailer" 3
+        get_repo "highcanfly-club/hcfschedule" "helm/hcfschedule" 2
+        get_repo "highcanfly-club/helm-dashboard" "helm/helm-dashboard" 2
+        get_repo "highcanfly-club/helm-roundecubemail" "roundcube" 1
+        get_repo "highcanfly-club/odoo-bitnami-custom" "helm/odoo" 2
+        get_repo "highcanfly-club/pretix" "helm/pretix" 2
+        get_repo "highcanfly-club/ubuntu-w64build" "helm/ubuntu-w64build" 2
+        get_repo "ismogroup/dolidock" "helm/dolidock" 2
 fi
 helm repo index repo/ --url=https://helm-repo.highcanfly.club
 # needs to install repo-html plugin;
