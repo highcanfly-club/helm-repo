6fd3eda08f515717c13ccbaeeb080ee9435999d8
LE MEILLAT <165936401+aeltorio@users.noreply.github.com>
2026-08-15 17:41:16 +0200

    Add sctg-claw repository to sync-charts script

--- sync-charts.sh
+++ sync-charts.sh
@@ -97,6 +97,7 @@ else
         get_repo "sctg-development/rallly" "helm/rallly" 2
         get_repo "sctg-development/tokeisrv" "helm/tokeisrv" 2
         get_repo "sctg-development/ollama-k8s" "ollama-helm" 1 HEAD ollama
+        get_repo "sctg-development/sctg-claw" "sctg-claw" 1
 fi
 
 # clean up
